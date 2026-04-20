83edcc19a3729ba79630b34f274345065013aa50
Shreyash Jaiswal <shreyash5709@gmail.com>
2026-04-20 19:08:00 +0530

    Fix CORS origin configuration to support local and production environments

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"https://ai-powered-mock-interview-frontend.onrender.com",
+    origin:"http://localhost:5173" || "https://ai-powered-mock-interview-frontend.onrender.com",
     credentials:true
 }))
 
